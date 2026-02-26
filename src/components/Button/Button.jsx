336d1e80789e398b6b128a4df9c7e151fcbc5b9a
Mariela Valerio <139282125+marivalerio104@users.noreply.github.com>
2026-02-25 19:31:55 -0600

    Add Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./Button.css";
+
+export default function Button({ children, to, onClick, type = "button", variant = "primary"}) {
+  const className = `btn-${variant}`;
+
+  // If "to" exists, render Link
+  if (to) {
+    return <Link to={to} className={className}>
+      {children}
+    </Link>;
+  }
+
+  // Otherwise render normal button
+  return <button type={type} onClick={onClick} className={className}>
+    {children}
+  </button>;
+}
