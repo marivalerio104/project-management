b94bc93b59c5aae03564b5b71cae4c1d6da80bc8
Mariela Valerio <139282125+marivalerio104@users.noreply.github.com>
2026-02-25 19:32:18 -0600

    Add Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,12 @@
 import "./Home.css"
+import logo from "../../assets/logo.png";
+import Button from "../../components/Button/Button";
 
 export default function Home() {
-  return <div id="home">Home</div>
+  return <div id="home">
+    <img src={logo} alt="Project Management Logo" />
+    <h2>No Project Selected</h2>
+    <p>Select a project or get started with a new one.</p>
+    <Button>Create Project</Button>
+  </div>
 }
