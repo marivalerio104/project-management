db9de3ced49b4ce96016fdd9c6bc4e717d279118
Mariela Valerio <139282125+marivalerio104@users.noreply.github.com>
2026-02-27 13:04:11 -0600

    Add delete project

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,16 +1,27 @@
 import "./Project.css";
 import Button from "../../components/Button/Button";
-import { useParams } from 'react-router-dom';
+import ConfirmationModal from "./components/ConfirmationModal/ConfirmationModal";
+import { useRef } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function Project({ projects, setProjects }) {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { name, description, dueDate, tasks } = projects.find(p => p.id === id);
+  const confirmationModal = useRef();
+
+  function handleDeleteProject() {
+    setProjects(projects.filter(p => p.id !== id));
+    toast.success("Project deleted successfully.");
+    navigate("/");
+  }
 
   return <>
     <div id="project">
       <header>
         <h2>{name}</h2>
-        <Button variant="secondary">
+        <Button variant="secondary" onClick={() => confirmationModal.current.showModal()}>
           Delete
         </Button>
       </header>
@@ -21,8 +32,13 @@ export default function Project({ projects, setProjects }) {
       <ul>
         {tasks.map(task => <li key={task.id}>
           {task.task}
+
         </li>)}
       </ul>
     </div>
+
+    <ConfirmationModal ref={confirmationModal} onConfirm={handleDeleteProject}
+      description="Are you sure you want to delete this project?"
+    />
   </>
 }
